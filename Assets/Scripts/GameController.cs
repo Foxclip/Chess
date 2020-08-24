1f09a4c92dd22b2b3ce0e7759dad145a1e3e9b15
Foxclip <destroyApp2@gmail.com>
2020-08-24 21:46:20 +1000

    Создание объектов клеток

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
 
+    public GameObject board;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Создание объектов для клеток
+        string letters = "abcdefgh";
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 1; y <= 8; y++)
+            {
+                GameObject newCell = new GameObject(letters[x].ToString() + y.ToString());
+                newCell.transform.parent = board.transform.Find("cells");
+                float halfCell = (float)(1.0 / 16.0);
+                float cellX = (float)(x / 8.0) + halfCell;
+                float cellY = (float)(y / 8.0) - halfCell;
+                newCell.transform.localPosition = new Vector3(cellX, cellY);
+            }
+        }
     }
 
     // Update is called once per frame
