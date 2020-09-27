e7e5dcfe2f22bdc4eda0a2a8603ffefd250f9fb1
Foxclip <destroyApp2@gmail.com>
2020-09-27 16:11:22 +1000

    Исчезание красной линии

--- Assets/Scripts/Animations/RedLineAnimation.cs
+++ Assets/Scripts/Animations/RedLineAnimation.cs
@@ -6,13 +6,21 @@ using UnityEngine;
 public class RedLineAnimation : MonoBehaviour
 {
     /// <summary>
-    /// Длительность анимации
+    /// Длительность фазы перемещения.
     /// </summary>
-    public float duration = 1.0f;
+    public float drawDuration = 1.0f;
+    /// <summary>
+    /// Длительность фазы исчезания.
+    /// </summary>
+    public float fadeoutDuration = 1.0f;
     /// <summary>
     /// Нелинейное изменение скорости: при маленьких значениях анимация замедляется, при больших - ускоряется
     /// </summary>
-    public float power = 0.2f;
+    public float power = 0.3f;
+    /// <summary>
+    /// Нелинейность исчезания.
+    /// </summary>
+    public float fadeoutPower = 1.0f;
     /// <summary>
     /// Время в секундах, прошедшее с начала анимации
     /// </summary>
@@ -30,30 +38,59 @@ public class RedLineAnimation : MonoBehaviour
     /// </summary>
     private bool active = false;
     /// <summary>
+    /// Стадия исчезания.
+    /// </summary>
+    private bool fadeoutPhase = false;
+    /// <summary>
     /// Вызывется при завершении анимации
     /// </summary>
     private Action finishedCallback;
+    /// <summary>
+    /// Материал объекта.
+    /// </summary>
+    private Material material;
 
     public void FixedUpdate()
     {
         if(active)
         {
             timePassed += Time.fixedDeltaTime;
-            if(timePassed > duration)
+            // Переход к фазе исчезания
+            if(!fadeoutPhase && (timePassed > drawDuration))
             {
-                timePassed = duration;
-                active = false;
+                fadeoutPhase = true;
+                timePassed = drawDuration;
                 finishedCallback();
             }
-            float completionPercentage = timePassed / duration;
-            float nonlinear = Mathf.Pow(completionPercentage, power);
-            Vector3 diff = endPos - beginPos;
-            Vector3 diffScaled = diff * nonlinear;
-            Vector3 diffScaledHalf = diffScaled / 2.0f;
-            Vector3 newPos = beginPos + diffScaledHalf;
-            Vector3 newPosAdjusted = new Vector3(newPos.x, newPos.y, 0.5f);
-            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, diffScaled.magnitude);
-            transform.position = newPosAdjusted;
+            // Конец анимации
+            if(fadeoutPhase && (timePassed > drawDuration + fadeoutDuration))
+            {
+                timePassed = drawDuration + fadeoutDuration;
+                active = false;
+            }
+            // Фаза перемещения
+            if(!fadeoutPhase)
+            {
+                float completionPercentage = timePassed / drawDuration;
+                float nonlinear = Mathf.Pow(completionPercentage, power);
+                Vector3 diff = endPos - beginPos;
+                Vector3 diffScaled = diff * nonlinear;
+                Vector3 diffScaledHalf = diffScaled / 2.0f;
+                Vector3 newPos = beginPos + diffScaledHalf;
+                Vector3 newPosAdjusted = new Vector3(newPos.x, newPos.y, 0.5f);
+                // Меняем масштаб и позицию объекта
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, diffScaled.magnitude);
+                transform.position = newPosAdjusted;
+            }
+            // Фаза исчезания
+            else
+            {
+                float completionPercentage = (timePassed - drawDuration) / fadeoutDuration;
+                float nonlinear = Mathf.Pow(completionPercentage, fadeoutPower);
+                float nonlinearReversed = 1 - nonlinear;
+                // Меняем прозрачность материала
+                material.SetColor("_Color", new Color(1.0f, 0.0f, 0.0f, nonlinearReversed));
+            }
         }
     }
 
@@ -69,10 +106,10 @@ public class RedLineAnimation : MonoBehaviour
         this.finishedCallback = finishedCallback;
         timePassed = 0.0f;
         active = true;
+        material = GetComponent<MeshRenderer>().material;
         // Ставим объект в нужное место
         transform.position = beginPos;
         // Сразу считаем угол
         transform.LookAt(endPos);
-        Debug.Log(transform.rotation.eulerAngles);
     }
 }
